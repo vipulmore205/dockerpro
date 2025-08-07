2293f5b4f9f9d36cdc8ef68c80015ac9c5aef46a
vipulmore205 <vipulmore205@gmail.com>
2025-08-07 13:03:51 +0530

    Update file.py

--- file.py
+++ file.py
@@ -1,26 +1,4 @@
-import random
-import qrcode
-import qrcode.constants
-lower="abcdefghijklmnopqrstuvwxyz"
-upper="ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-Nos="0123456789"
-sym="!@#$%^&*()_-+[]"
-
-allchar = lower + upper + Nos + sym
-length=int(input("Enter the length: "))
-hello = ''.join(random.choices(allchar, k=length))
-print('Generated password:',hello)
-
-data=hello
-qr= qrcode.QRCode(
-    version=1,
-    error_correction=qrcode.constants.ERROR_CORRECT_L,
-    box_size=10,
-    border=4,
-)
-qr.add_data(data)
-qr.make(fit=True)
-
-img = qr.make_image(fill_color="black",back_color="white")
-img.save("qrcode.png","PNG")
-img.show()
+a=55
+b=22
+c=a+b
+print(c)
